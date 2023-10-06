bc9e0b1f501bc3daab74c6f849af0c0932045861
vandana <vandana.edha.494@gmail.com>
2023-10-06 14:08:37 +0530

    thelast

---
+++ pages/FSAMahadevpura.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import styles from './FSAMahadevpura.module.css';
+
+
+function Mahadevpura() {
+    return (
+        <div>
+            <Header />
+          <h1>FSA Mahadevpura</h1>  
+            
+        </div>
+    );
+}
+
+export default Mahadevpura;
---
+++ pages/FSAWhitefield.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../components/Header';
+import styles from './FSAWhitefield.module.css';
+import Footer from '../components/Footer';
+
+function Whitefield() {
+    return (
+        <div>
+            <Header />
+            <h1>FSA WHitefield </h1>
+            
+            {/* Content for your component can go here */}
+            
+             
+        </div>
+    );
+}
+
+export default Whitefield;
